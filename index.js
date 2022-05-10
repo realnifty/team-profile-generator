df21f401b1d625b8c31581c7eb6d324d040b8ff6
gordo <rogelioalbertopedraza@gmail.com>
2022-05-09 20:46:18 -0500

    employee prompt complete

--- index.js
+++ index.js
@@ -39,4 +39,49 @@ const mngrPrompt = () => {
     })
 };
 
-mngrPrompt();
+const employeePrompt = () => {
+    console.log('Add new employees.');
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Choose a role for your employee.',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is this employee's name?`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `What is this employee's ID?`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `What is this employee's email address?`,
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `What is this employee's GitHub username?`,
+        },
+        {
+            when: (inputData) => inputData.role === 'Intern',
+            type: 'input',
+            name: 'school',
+            message: 'Where did this employee go to school?',
+        },
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Would you like to an additional employee?',
+            default: false
+        }
+    ])
+};
+
+employeePrompt();
