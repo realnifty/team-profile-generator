04ec1cda6d76b898ebecc2e6cecaa38a5a3b7f07
gordo <rogelioalbertopedraza@gmail.com>
2022-05-09 20:16:29 -0500

    manager inquirer prompt complete

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ const genHTML = require('./src/genHTML')
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
+
+const mngrPrompt = () => {
+    return inquirer.prompt ([
+        {
+            type:'input',
+            name: 'name',
+            message: `Enter your team manager's name.`
+        },
+        {
+            type:'input',
+            name: 'id',
+            message: `Enter your team manager's ID.`
+        },
+        {
+            type:'input',
+            name: 'email',
+            message: `Enter your team manager's email address.`
+        },
+        {
+            type:'input',
+            name: 'officeNumber',
+            message: `Enter your team manager's office number.`
+        }
+    ])
+};
