c2c112f75a895180b6073c74f1f6672d390e7a2e
gordo <rogelioalbertopedraza@gmail.com>
2022-05-09 20:00:59 -0500

    add node modules, HTML generator, team profiles to index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const genHTML = require('./src/genHTML')
+
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
